8f163b2cc28e4c255ef5093758e666479d5e8f5f
AhmadT <ahmadt@gmail.com>
2020-07-24 06:56:01 +0300

    cascade

--- database/migrations/2020_07_06_084511_create_application_user_table.php
+++ database/migrations/2020_07_06_084511_create_application_user_table.php
@@ -15,8 +15,8 @@ class CreateApplicationUserTable extends Migration
     {
         Schema::create('application_user', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('application_id')->constrained('applications');
-            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('application_id')->constrained('applications')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
             $table->unique(['application_id', 'user_id']);
             $table->index('application_id');
